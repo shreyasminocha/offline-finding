aa0d9d1df76daaae01f770546d1bd04d693ca555
Shreyas Minocha <sm-git@riseup.net>
2025-02-19 01:45:16 -0500

    Add more OF tests

--- offline_finding/src/accessory.rs
+++ offline_finding/src/accessory.rs
@@ -37,19 +37,12 @@ impl Accessory {
         let mut master_beacon_symmetric_key = [0; 32];
         csprng.fill_bytes(&mut master_beacon_symmetric_key);
 
-        let mut accessory = Accessory {
-            master_beacon_private_key: master_beacon_private_key.clone(),
-            master_beacon_symmetric_key,
-            current_private_key: master_beacon_private_key,
-            current_symmetric_key: master_beacon_symmetric_key,
-        };
-
-        // important: generate initial ephemeral keys
-        accessory.rotate_keys();
+        let accessory = Self::new(master_beacon_private_key, master_beacon_symmetric_key);
 
         let mb_private_key = accessory.master_beacon_private_key.clone();
         let mb_symmetric_key = accessory.master_beacon_symmetric_key;
 
+        // TODO: reconsider this return type
         (accessory, mb_private_key, mb_symmetric_key)
     }
 
@@ -89,5 +82,93 @@ impl Accessory {
 
     pub fn get_current_public_key(&self) -> OfflineFindingPublicKey {
         OfflineFindingPublicKey::from(&self.current_private_key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use const_decoder::{decode, Decoder};
+    use p224::elliptic_curve::sec1::ToEncodedPoint;
+
+    use super::*;
+
+    #[test]
+    fn test_returned_mb_keys_match_those_in_struct() {
+        let (accessory, mb_private_key, mb_symmetric_key) =
+            Accessory::random(&mut rand::rngs::OsRng);
+
+        assert_eq!(mb_private_key, accessory.master_beacon_private_key);
+        assert_eq!(mb_symmetric_key, accessory.master_beacon_symmetric_key);
+    }
+
+    #[test]
+    fn test_ephemeral_keys_not_same_as_master_ones() {
+        let (accessory, mb_private_key, mb_symmetric_key) =
+            Accessory::random(&mut rand::rngs::OsRng);
+
+        assert_ne!(mb_private_key, accessory.current_private_key);
+        assert_ne!(mb_symmetric_key, accessory.current_symmetric_key);
+    }
+
+    #[test]
+    fn test_public_key_matches_current_private_key() {
+        let (accessory, _, _) = Accessory::random(&mut rand::rngs::OsRng);
+        let accessory_public_key: [u8; 28] = accessory.get_current_public_key().into();
+
+        let actual_public_key_point = accessory
+            .current_private_key
+            .public_key()
+            .to_encoded_point(true);
+        let actual_public_key = actual_public_key_point.as_bytes();
+        let actual_public_key_without_sign_bit = &actual_public_key[1..];
+
+        assert_eq!(
+            actual_public_key_without_sign_bit,
+            accessory_public_key.as_slice()
+        );
+    }
+
+    #[test]
+    fn test_key_rotation_matches_that_of_apple_airtags() {
+        let mb_private_key = SecretKey::from_slice(&decode!(
+            Decoder::Base64,
+            b"KioqKioqKioqKioqKioqKioqKioqKioqKioqKg=="
+        ))
+        .unwrap();
+        let mb_symmetric_key = decode!(
+            Decoder::Base64,
+            b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+        );
+
+        let mut accessory = Accessory::new(mb_private_key, mb_symmetric_key);
+
+        // expected values from https://github.com/malmeloo/FindMy.py
+
+        let actual: [u8; 28] = accessory.get_current_public_key().into();
+        assert_eq!(
+            actual,
+            decode!(Decoder::Base64, b"77HRu4h48OgZIPO+eV9FOE8nPRZqpXT/FGbBAA==")
+        );
+
+        accessory.rotate_keys();
+        let actual: [u8; 28] = accessory.get_current_public_key().into();
+        assert_eq!(
+            actual,
+            decode!(Decoder::Base64, b"uoIdNzCdygG33VdHW9Sq6bnXqpuiT71qBZHuGA==")
+        );
+
+        accessory.rotate_keys();
+        let actual: [u8; 28] = accessory.get_current_public_key().into();
+        assert_eq!(
+            actual,
+            decode!(Decoder::Base64, b"qfsoe/hOu0Kbtkpbfs4dSnPmuGFZfHno1nNnRw==")
+        );
+
+        accessory.rotate_keys();
+        let actual: [u8; 28] = accessory.get_current_public_key().into();
+        assert_eq!(
+            actual,
+            decode!(Decoder::Base64, b"mR9Q7KjvRPUt56j6vZFwgtOHV1+tT6hOcBbjEA==")
+        );
     }
 }
--- offline_finding/src/public_key.rs
+++ offline_finding/src/public_key.rs
@@ -1,6 +1,8 @@
 use p224::{elliptic_curve::sec1::ToEncodedPoint, SecretKey};
 use sha2::{Digest, Sha256};
 
+const TWO_MOST_SIGNIFICANT_BITS_MASK: u8 = 0b11000000;
+
 pub struct OfflineFindingPublicKey([u8; 28]);
 
 impl OfflineFindingPublicKey {
@@ -11,7 +13,7 @@ impl OfflineFindingPublicKey {
             .expect("six < twenty-eight")
             .try_into()
             .expect("there are exactly six elements in the slice");
-        addr_bytes_be[0] |= 0b11000000;
+        addr_bytes_be[0] |= TWO_MOST_SIGNIFICANT_BITS_MASK;
 
         addr_bytes_be
     }
@@ -23,8 +25,7 @@ impl OfflineFindingPublicKey {
             0x12,       // Offline finding
             25,         // Length of following data
             0b11100000, // Status (e.g. battery level)
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, // last 22 key bytes
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // key[-22:]
             0, // first two bits of key
             0, // Hint. Indicates something about the lost device? 0x00 for iOS reports
         ];
@@ -56,6 +57,12 @@ impl From<&SecretKey> for OfflineFindingPublicKey {
     }
 }
 
+impl From<OfflineFindingPublicKey> for [u8; 28] {
+    fn from(value: OfflineFindingPublicKey) -> Self {
+        value.0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use const_decoder::{decode, Decoder};
@@ -64,11 +71,62 @@ mod tests {
 
     #[test]
     fn test_to_ble_address_bytes_be() {
-        let public_key: [u8; 28] =
-            decode!(Decoder::Base64, b"/j3eaoofkmPIV4hAJTIh2qmE9s1W3Y4PoBoohg==");
+        let public_key = decode!(Decoder::Base64, b"/j3eaoofkmPIV4hAJTIh2qmE9s1W3Y4PoBoohg==");
         let of_public_key = OfflineFindingPublicKey(public_key);
         let mac = of_public_key.to_ble_address_bytes_be();
 
         assert_eq!(mac.as_slice(), decode!(Decoder::Hex, b"FE3DDE6A8A1F"));
+        assert_eq!(
+            mac[0] & TWO_MOST_SIGNIFICANT_BITS_MASK,
+            TWO_MOST_SIGNIFICANT_BITS_MASK
+        );
+    }
+
+    #[test]
+    fn test_to_ble_advertisement_data() {
+        let public_key = [0; 28];
+        let of_public_key = OfflineFindingPublicKey(public_key);
+        let ad_data = of_public_key.to_ble_advertisement_data();
+
+        assert_eq!(ad_data[0..2], [0x4c, 0x00]);
+        assert_eq!(ad_data[2], 0x12);
+        assert_eq!(ad_data[3], 25);
+        assert_eq!(ad_data[27], public_key[0] >> 6);
+    }
+
+    #[test]
+    fn test_hash() {
+        let public_key = decode!(Decoder::Base64, b"/j3eaoofkmPIV4hAJTIh2qmE9s1W3Y4PoBoohg==");
+        let of_public_key = OfflineFindingPublicKey(public_key);
+
+        assert_eq!(
+            of_public_key.hash(),
+            decode!(
+                Decoder::Base64,
+                b"RwPKNxB/wNDVZuQ8UEmKb2KHdakTHDNPTEvZ2kxRFvQ="
+            )
+        );
+    }
+
+    #[test]
+    fn test_reconstructed_public_key_matches() {
+        let public_key = [0; 28];
+        let of_public_key = OfflineFindingPublicKey(public_key);
+
+        let mac = of_public_key.to_ble_address_bytes_be();
+        let ad_data = of_public_key.to_ble_advertisement_data();
+
+        let thing = ad_data[27] << 6;
+        dbg!(thing, mac[0], mac[0] & !TWO_MOST_SIGNIFICANT_BITS_MASK);
+
+        let reconstructed_public_key = [
+            &[(ad_data[27] << 6) | (mac[0] & !TWO_MOST_SIGNIFICANT_BITS_MASK)],
+            &mac[1..6],
+            &ad_data[5..27],
+        ]
+        .concat();
+
+        assert_eq!(reconstructed_public_key.len(), 28);
+        assert_eq!(public_key.to_vec(), reconstructed_public_key);
     }
 }
